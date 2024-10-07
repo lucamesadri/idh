25e62bab07593183a9dc7ec0fd1d479cabc8bf94
Luca Vinícius Mesadri <101848988+lucamesadri@users.noreply.github.com>
2024-10-07 12:10:05 -0300

    Update Continente.java

--- idh/src/main/java/br/edu/univille/poo/idh/entity/Continente.java
+++ idh/src/main/java/br/edu/univille/poo/idh/entity/Continente.java
@@ -1,13 +1,19 @@
 package br.edu.univille.poo.idh.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @NoArgsConstructor
 @Entity
 public class Continente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
 }
